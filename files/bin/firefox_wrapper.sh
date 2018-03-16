47e45253bf3a3df43146542618412b5512b0a9e6
Gunnar Andersson <gandersson@genivi.org>
2018-03-16 14:15:20 +0100

    firefox: open standard pages right away

---
+++ files/bin/firefox_wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Open standard tabs right away
+/usr/bin/firefox \
+     https://genivi.webex.com \
+     https://at.projects.genivi.org/wiki \
+     https://at.projects.genivi.org/wiki/x/rIP0 \
+     https://zoom.us/
